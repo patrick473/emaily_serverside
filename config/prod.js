aec24b41eda5b3fedcf78824ca3d21c821e58e79
patrick kottman <patkottman@gmail.com>
2018-07-16 21:23:09 +0200

    possible to log out user

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports ={
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURL: process.env.MONGO_URL,
+    cookieKey: process.env.COOKIE_KEY,
+}
