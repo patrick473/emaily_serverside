d1c8332c972992cdb1da56cbc034ab0f27f01cb0
patrick kottman <patkottman@gmail.com>
2018-07-17 22:23:23 +0200

    further implementation of authentication

---
+++ client/src/components/App.js
@@ -0,0 +1,43 @@
+import React,{Component} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {connect} from "react-redux";
+
+import * as actions from '../actions';
+
+import Header from "./Header";
+const Dashboard = () => <h2> Dashboard</h2>;
+const SurveyNew = () => <h2> SurveyNew</h2>;
+const Landing = () => <h2> Landing</h2>;
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+class App extends Component {
+    componentDidMount() {
+      this.props.fetchUser();
+    }
+    
+    render() {
+        return (
+            <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+
+            <Route path="/surveys" component={Dashboard} />
+
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+        );
+    }
+}
+
+
+export default connect(mapStateToProps,actions)(App);
