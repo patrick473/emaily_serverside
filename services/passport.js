2e603c90dcb17d36e71c109c59e2829301514bf6
patrick kottman <patkottman@gmail.com>
2018-07-17 18:03:54 +0200

    implement better async call handling

--- services/passport.js
+++ services/passport.js
@@ -20,19 +20,19 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true,
-    },  (accessToken, refreshToken,profile,done) => {
+    },
+    async (accessToken, refreshToken,profile,done) => {
        
-        User.findOne({googleID: profile.id})
-        .then((existingUser)=>{
-            if(existingUser){
-                done(null,existingUser);
-            }
-            else{
-                new User({ googleID: profile.id})
-                .save()
-                .then(user => done(null,user));
-            }
-        });
+        const existingUser = await User.findOne({googleID: profile.id});
+
+        if(existingUser){
+            return done(null,existingUser);
+        }
+        
+        const user = await new User({ googleID: profile.id}).save();
+        done(null,user);
+    
+       
         
        
     })
